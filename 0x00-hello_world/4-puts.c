160c2597b89a3c12afe747ce9885202dea4e9bdb
DrOgeed <ogetoedwin3@gmail.com>
2023-03-16 08:09:48 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program  that prints a line with puts function
+ * Return: 0 (This means a success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilimgual puzzle");
+return (0);
+}
